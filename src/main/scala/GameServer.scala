c6de1eefc5941d339374ba2e3f3f65dbdb05cc7a
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-11-22 23:36:06 -0600

    Updated port

--- src/main/scala/GameServer.scala
+++ src/main/scala/GameServer.scala
@@ -92,7 +92,7 @@ object GameServer extends App with JsonSupport{
       }
     })
 
-  val server = Http().newServerAt("localhost", 9090).bind(route)
+  val server = Http().newServerAt("localhost", 80).bind(route)
   server.map { _ =>
     logger.info("Successfully started on localhost:9090 ")
   } recover { case ex =>
